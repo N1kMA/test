2f1931a28f859f34529140c6d03dd549608f04e4
ololoev783@gmail.com <ololoev783@gmail.com>
2023-07-02 18:19:06 +0000

    not changed

--- stages/ads_performance.sql
+++ stages/ads_performance.sql
@@ -1,8 +1,14 @@
-select
-    channel,
-    sum(spend) / sum(total_conversions) as conversion_cost,
-    sum(spend) / sum(engagements) as cost_per_engage,
-    sum(impressions) as impressions_by_channel,
-    sum(spend) / sum(clicks) as cpc
-from {{ ref("paid_ads__basic_performance") }}
+
+select channel, 
+sum(spend)/sum(total_conversions) as Conversion_cost,
+sum(spend)/sum(engagements) as Cost_per_engage,
+sum(impressions) as Impressions_by_channel,
+sum(spend)/sum(clicks) as CPC
+from {{ ref('paid_ads__basic_performance') }}
 group by channel
+
+
+
+
+
+
